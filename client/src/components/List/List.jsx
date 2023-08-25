19eda892b09f3b5b89effa1987a282281c1d3e9f
boyka <abdelrahmanzaki747@gmail.com>
2023-08-25 23:47:17 +0300

    Products page

---
+++ client/src/components/List/List.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import "./List.scss"
+import Card from '../Card/Card'
+
+const List = () => {
+    const data = [
+        {
+            id: 1,
+            img: "https://images.pexels.com/photos/6839163/pexels-photo-6839163.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            img2: "https://images.pexels.com/photos/6839183/pexels-photo-6839183.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+            title: "Best Style In London",
+            isNew: true,
+            oldPrice: 19,
+            price: 25
+        },
+        {
+            id: 2,
+            img: "https://images.pexels.com/photos/11354160/pexels-photo-11354160.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+            img2: "https://images.pexels.com/photos/11354154/pexels-photo-11354154.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+            title: "Best Style In London",
+            isNew: false,
+            oldPrice: 19,
+            price: 25
+        },
+        {
+            id: 3,
+            img: "https://images.pexels.com/photos/12551562/pexels-photo-12551562.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+            img2: "https://images.pexels.com/photos/12551552/pexels-photo-12551552.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+            title: "Best Style In London",
+            isNew: true,
+            oldPrice: 19,
+            price: 25
+        },
+        {
+            id: 4,
+            img: "https://images.pexels.com/photos/17433110/pexels-photo-17433110/free-photo-of-young-woman-posing-in-an-elegant-black-dress.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+            img2: "https://images.pexels.com/photos/17433112/pexels-photo-17433112/free-photo-of-young-woman-posing-in-an-elegant-black-dress.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+            title: "Best Style In London",
+            isNew: false,
+            oldPrice: 19,
+            price: 25
+        },
+    ]
+    return (
+        <div className='list'>
+            {data?.map(item => (
+                <Card item={item} key={item.id}/>
+            ))}
+        </div>
+    )
+}
+
+export default List
--- client/src/pages/Products/Products.jsx
+++ client/src/pages/Products/Products.jsx
@@ -1,8 +1,62 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import "./Products.scss"
+import List from '../../components/List/List'
+import { useParams } from 'react-router-dom'
 const Products = () => {
+
+  const catId = parseInt(useParams().id)
+  const [maxPrice, setMaxPrice] = useState(5000)
+  const [sort,setSort]=useState(null)
+
   return (
-    <div>Products</div>
+    <div className='products'>
+      <div className="left">
+        <div className="filterItem">
+          <h2>Product Categories</h2>
+          <div className="inputItem">
+            <input type="checkbox" id='1' value={1} />
+            <label htmlFor="1">Shoes</label>
+          </div>
+          <div className="inputItem">
+            <input type="checkbox" id='2' value={1} />
+            <label htmlFor="2">shirts</label>
+          </div>
+          <div className="inputItem">
+            <input type="checkbox" id='3' value={1} />
+            <label htmlFor="3">shorts</label>
+          </div>
+        </div>
+
+        <div className="filterItem">
+          <h2>Filter by price</h2>
+          <div className="priceRange">
+            <span>50</span>
+            <input type="range" name="" id="" min={50} max={5000} onChange={(e) => setMaxPrice(e.target.value)} />
+            <span>{maxPrice}</span>
+          </div>
+        </div>
+
+        <div className="filterItem">
+          <h2>Sort by</h2>
+          <div className="inputItem">
+            <input type="radio" id='asc' name='price' onClick={e=>setSort("asc")}/>
+            <label htmlFor="asc">Price (Lowest First)</label>
+          </div>
+          <div className="inputItem">
+            <input type="radio" id='desc' name='price' onClick={e => setSort("desc")} />
+            <label htmlFor="desc">Price (Highest First)</label>
+          </div>
+        </div>
+      </div>
+      <div className="right">
+        <img
+          className="catImg"
+          src="https://images.pexels.com/photos/1074535/pexels-photo-1074535.jpeg?auto=compress&cs=tinysrgb&w=1600"
+          alt=""
+        />
+        <List carId={catId} maxPrice={maxPrice} sort={sort}/>
+      </div>
+    </div>
   )
 }
 
